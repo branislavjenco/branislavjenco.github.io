f03e78600ec924d0c3f83e24482f33ff2f200581
Branislav Jenco <branislav.jenco@sesam.io>
2024-10-01 21:46:39 +0200

    shared head element

--- build.py
+++ build.py
@@ -20,6 +20,7 @@ env = Environment(
 )
 
 footer_html = env.get_template("footer.html").render()
+head_html = env.get_template("head.html").render()
 index_template = env.get_template("index.html")
 about_template = env.get_template("about.html")
 post_template = env.get_template("mainpage_post.html")
@@ -51,19 +52,18 @@ for filename in posts_to_publish:
             post_html = markdown.markdown(post_md, extensions=extensions)
             post_rendered = post_template.render(title=title_link, date=date, body=post_html)
             posts_html = posts_html + post_rendered
-            single_post_rendered = single_post_template.render(title=title, date=date, body=post_html, footer=footer_html)
+            single_post_rendered = single_post_template.render(title=title, date=date, body=post_html, head=head_html, footer=footer_html)
             with open(f"{build_folder}/{filename.replace('.md','')}.html", 'w') as f:
                 f.write(single_post_rendered)
-
 
 with open(f"about.md") as md_file:
     about_html = markdown.markdown(md_file.read(), extensions=extensions)
     with open(f"{build_folder}/about.html", 'w') as html_file:
-        result = about_template.render(about=about_html, footer=footer_html)
+        result = about_template.render(about=about_html, head=head_html, footer=footer_html)
         html_file.write(result)
 
 with open(f"{build_folder}/index.html", 'w') as f:
-    result = index_template.render(posts=posts_html, footer=footer_html)
+    result = index_template.render(posts=posts_html, head=head_html, footer=footer_html)
     f.write(result)
 
 shutil.copytree("images", "build/images", dirs_exist_ok=True)
